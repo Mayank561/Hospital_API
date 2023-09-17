b11716a80f5cf090aab06dfc4658b10ab92be6f6
Mayank561 <108197241+Mayank561@users.noreply.github.com>
2023-09-17 18:58:56 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/Hospital_Api');
+mongoose.connect('mongodb://localhost:27017/Hospital_Api');
 const db = mongoose.connection;
 
 db.on('error',console.error.bind(console,'Error connecting to the db'));
